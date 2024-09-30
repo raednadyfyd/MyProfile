ca20e975e00479311f3e311ab7019fa2e14c78dd
Raed Nady <raed.feed.external@almentor.net>
2024-09-30 19:26:01 +0300

    adding services page design

--- src/app/user/services/services.component.ts
+++ src/app/user/services/services.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent {
-
+  services = [
+    {
+      icon: 'icon-bulb',
+      title: 'Innovative Ideas',
+      description: 'Separated they live in Bookmarksgrove right at the coast of the Semantics',
+      colorClass: 'color-1'
+    },
+    {
+      icon: 'icon-data',
+      title: 'Software',
+      description: 'Separated they live in Bookmarksgrove right at the coast of the Semantics',
+      colorClass: 'color-2'
+    },
+    {
+      icon: 'icon-phone3',
+      title: 'Application',
+      description: 'Separated they live in Bookmarksgrove right at the coast of the Semantics',
+      colorClass: 'color-3'
+    },
+    {
+      icon: 'icon-layers2',
+      title: 'Graphic Design',
+      description: 'Separated they live in Bookmarksgrove right at the coast of the Semantics',
+      colorClass: 'color-4'
+    },
+    {
+      icon: 'icon-data',
+      title: 'Software',
+      description: 'Separated they live in Bookmarksgrove right at the coast of the Semantics',
+      colorClass: 'color-5'
+    },
+    {
+      icon: 'icon-phone3',
+      title: 'Application',
+      description: 'Separated they live in Bookmarksgrove right at the coast of the Semantics',
+      colorClass: 'color-6'
+    }
+  ];
 }
