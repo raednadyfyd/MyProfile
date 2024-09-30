2803d4f910d278ba5b8e798b85c9bfb9d77004b0
Raed Nady <raed.feed.external@almentor.net>
2024-09-30 19:22:15 +0300

    adding about page design

--- src/app/user/about/about.component.ts
+++ src/app/user/about/about.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  title = "Who Am I?";
+  introduction = "Hi I'm Raed Nady. Senior dot net developer.";
+  additionalInfo = "Expirence with +6 of years in dot net field";
+  
+  services = [
+    { icon: 'icon-bulb', title: 'Graphic Design', colorClass: 'color-1', animationEffect: 'fadeInLeft' },
+    { icon: 'icon-globe-outline', title: 'Web Design', colorClass: 'color-2', animationEffect: 'fadeInRight' },
+    { icon: 'icon-data', title: 'Software', colorClass: 'color-3', animationEffect: 'fadeInTop' },
+    { icon: 'icon-phone3', title: 'Application', colorClass: 'color-4', animationEffect: 'fadeInBottom' }
+  ];
+
+  hireMessage = "I am happy to know you that 300+ projects done successfully!";
 }
