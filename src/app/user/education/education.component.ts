c3d459ad8eae727de3694ee16200bb5933c94c34
Raed Nady <raed.feed.external@almentor.net>
2024-09-30 21:12:37 +0300

    adding education page design

--- src/app/user/education/education.component.ts
+++ src/app/user/education/education.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./education.component.scss']
 })
 export class EducationComponent {
+   
+  educationData = [
+    { id: 1, 
+      isCollapsed: true, 
+      buttonColor: 'gray',
+      title: 'Master Degree Graphic Design',
+      description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.',
+   
+     },
+    { id: 2, 
+      isCollapsed: true,
+      buttonColor: 'gray',
+      title: 'Bachelor Degree of Computer Science',
+      description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.',
+    
+     },
+    { id: 3, 
+      isCollapsed: true,
+       buttonColor: 'gray',
+      title: 'Diploma in Information Technology',
+      description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.',
+    
+     },
+     {
+     id: 4, 
+      isCollapsed: true,
+       buttonColor: 'gray',
+       title: 'High School Secondary Education',
+       description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.',
+     }
+  ];
 
+  toggleCollapse(item: any) {
+    item.isCollapsed = !item.isCollapsed;
+    item.buttonColor = item.isCollapsed ? 'gray' : 'blue'; // Change color on toggle
+  }
 }
