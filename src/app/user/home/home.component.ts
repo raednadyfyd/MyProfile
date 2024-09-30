12b23bf3f44d8b25c48f55c75b7619c9274b3030
Raed Nady <raed.feed.external@almentor.net>
2024-09-30 18:39:32 +0300

    adding home page design

--- src/app/user/home/home.component.ts
+++ src/app/user/home/home.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  interval = 3000;
+  slides = [
+    {
+      imageUrl: 'assets/images/raed.png',
+      title: "Hi! I'm Raed",
+      description: 'Made by Angular',
+      buttonText: 'Download CV',
+      buttonIcon: 'icon-download4',
+      buttonLink: '#',
+    },
+    {
+      imageUrl: 'assets/images/avatar.png',
+      title: 'I am a full stack',
+      description: '100% HTML5 Bootstrap templates made by colorlib.com',
+      buttonText: 'View Portfolio',
+      buttonIcon: 'icon-briefcase3',
+      buttonLink: '#',  
+    }
+  ];
 }
