e5efe86a0e5441ee2c178b474086334536f24eb4
Raed Nady <raed.feed.external@almentor.net>
2024-09-30 19:28:46 +0300

    adding skills page design

--- src/app/user/skills/skills.component.ts
+++ src/app/user/skills/skills.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
-
+  skills = [
+    { name: 'Photoshop', percentage: 75, colorClass: 'color-1', animateEffect: 'fadeInLeft' },
+    { name: 'jQuery', percentage: 60, colorClass: 'color-2', animateEffect: 'fadeInRight' },
+    { name: 'HTML5', percentage: 85, colorClass: 'color-3', animateEffect: 'fadeInLeft' },
+    { name: 'CSS3', percentage: 90, colorClass: 'color-4', animateEffect: 'fadeInRight' },
+    { name: 'WordPress', percentage: 70, colorClass: 'color-5', animateEffect: 'fadeInLeft' },
+    { name: 'SEO', percentage: 80, colorClass: 'color-6', animateEffect: 'fadeInRight' }
+  ];
 }
